5d55bafd7ae0c6a7e15ce88a99f30a0a1a2cfbca
tinaleios <tianleios@163.com>
2017-12-19 20:37:36 +0800

    test

--- src/main/java/grammar/thread/Playground.java
+++ src/main/java/grammar/thread/Playground.java
@@ -1,7 +1,17 @@
 package grammar.thread;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * Created by tianlei on 2017/十二月/19.
  */
 public class Playground {
+
+    public static void main(String[] args) {
+
+//        用于并发的类
+        AtomicInteger atomicInteger = new AtomicInteger();
+//        AtomicIntegerArray
+
+    }
 }
